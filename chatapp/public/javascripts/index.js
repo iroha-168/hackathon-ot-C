561341029ae633d8bd67281b422d83009139a068
iroha-168 <chimosy21@gmail.com>
2020-11-12 19:25:59 +0900

    ログイン画面とチャット画面の実装

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,9 +3,18 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = '';
-    // ユーザ名が未入力でないかチェックする
+    const userName = getValue();
 
+    // ユーザ名が未入力でないかチェックする
+    if(userName == "") {
+        //ユーザー名を入力してくださいと出力
+        alert("ユーザー名を入力してください");
+    }
 
     $('form').submit();
+}
+
+function getValue() {
+    const userName = $('#userName').val();
+    return userName;
 }
