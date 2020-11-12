51ca43df2de81258a55cdadaff50d7ec016f6a18
S-syukatu <saiyutoki116@yahoo.co.jp>
2020-11-12 18:38:36 +0900

    enterEventChange

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,7 +2,8 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-    socket.on('', function (data) {
-
+    socket.on('enterMyselfEvent', function (data) {
+        // 画面上にメッセージを表示
+        $('#thread').prepend('<p>' + data + '</p>');
     });
 };
