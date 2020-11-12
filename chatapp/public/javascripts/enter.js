eb2c3c7b6369d3cf5fddd4a1f1d37b96eeac4e69
S-syukatu <saiyutoki116@yahoo.co.jp>
2020-11-12 21:09:58 +0900

    コメントに名前入れた

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// 入室メッセージをサーバに送信する
-
 // 入力されたユーザ名を取得する
-const userName = '';
+const userName = $('#userName').val();
 // 入室メッセージイベントを送信する
-
+//io.sockets.emit('enterMyselEvent',)
+// 入室メッセージをサーバに送信する
+socket.emit('enterMyselfEvent',userName +"さんが入室しました。")
 
 // サーバから受信した入室メッセージを画面上に表示する
 socket.on('enterMyselfEvent', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+    $('#thread').prepend('<p>' +userName+ '</p>');
 });
