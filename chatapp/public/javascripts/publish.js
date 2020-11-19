a3034966dc1696c781ea0a671587acccd174b13a
TetsuKawa <tetsu0513.non@docomo.ne.jp>
2020-11-19 14:55:41 +0900

    tui

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -6,10 +6,35 @@ function publish() {
     const userName = $('#userName').val();
     // 入力されたメッセージを取得
     const message = $('#message').val();
+    //空メッセージのチェック・エラー表示
+    if(message == ""){
+        alert("メッセージを入力してください");
+        return false;
+    }
+    //空白、改行のみのメッセージのチェック・エラー表示
+    let count = 0;
+    
+    for(let i = 0; i < message.length; i++){
+        if(message[i] == " " || message[i] == "　"){
+            continue;
+        }
+        else if(message[i] == "\n"){
+            continue;
+        }
+        else{
+            count++;
+            break;
+        }
+    }
+    if(count === 0){
+        alert("空白、改行以外の文字を入力してください");
+        return false;
+    }
+    
     // 投稿内容を送信
     socket.emit('sendMessageEvent',userName +"さん"+" : "+ message)
     //textBoxを空にする
-    message = $('#message').val("");
+    $('#message').val("");
 
     return false;
 }
