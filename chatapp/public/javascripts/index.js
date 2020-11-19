bc99b11a187ed73e5a894fd10987e9b84494ac12
TetsuKawa <tetsu0513.non@docomo.ne.jp>
2020-11-19 21:44:33 +0900

    database修正中

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -20,7 +20,7 @@ function enter() {
             return false;
         }
     })
-    
+    taskInsert();
     $('form').submit();
 }
 
@@ -28,3 +28,20 @@ function getValue() {
     const userName = $('#userName').val();
     return userName;
 }
+
+function taskInsert(){
+    var sqlite = require('sqlite3').verbose();                                          
+    var db = new sqlite.Database('test.sqlite');
+  
+    db.serialize(function() {
+  
+      // テーブルがなければ作成
+      db.run('CREATE TABLE IF NOT EXISTS task(id INT, content TEXT)');
+    
+      // プリペアードステートメントでデータ挿入
+      var stmt = db.prepare('INSERT INTO task VALUES(?,?)');
+      stmt.run([1, ""]);
+    });
+  
+    db.close();
+  }
